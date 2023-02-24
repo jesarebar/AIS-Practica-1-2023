2baec4739234805b81207ef93c64c509f2115b17
albertoam3 <albertoarcones3@gmail.com>
2023-02-24 10:16:17 +0100

    magicBook

--- src/test/java/es/codeurjc/ais/unit_test/bookTest.java
+++ src/test/java/es/codeurjc/ais/unit_test/bookTest.java
@@ -1,18 +1,7 @@
 package es.codeurjc.ais.unit_test;
 
-import es.codeurjc.ais.book.BookService;
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.devtools.restart.RestartScope;
-import static org.mockito.Mockito.*;
+
 
 public class bookTest {
-    @Test
-    public class magicTest(){
 
-        //Given
-        BookService book=mock(BookService.class);
-        //When
-
-        //Then
-    }
 }
--- src/test/java/es/codeurjc/ais/unit_test/magicTest.java
+++ src/test/java/es/codeurjc/ais/unit_test/magicTest.java
@@ -1,4 +1,19 @@
 package es.codeurjc.ais.unit_test;
 
+import es.codeurjc.ais.book.BookService;
+import org.junit.jupiter.api.Test;
+
+import static org.mockito.Mockito.mock;
+
 public class magicTest {
+
+    @Test
+    public class magicTest(){
+
+        //Given
+        BookService book=mock(BookService.class);
+        //When
+        when(book.findAll("Magic"))
+        //Then
+    }
 }
