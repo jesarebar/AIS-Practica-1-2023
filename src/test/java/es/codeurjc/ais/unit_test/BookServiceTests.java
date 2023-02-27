afd5ec7168511ae88882b78c31c517c82cbc38cb
albertoam3 <albertoarcones3@gmail.com>
2023-02-27 18:41:33 +0100

    test1 casi

--- src/test/java/es/codeurjc/ais/unit_test/BookServiceTests.java
+++ src/test/java/es/codeurjc/ais/unit_test/BookServiceTests.java
@@ -1,19 +1,41 @@
 package es.codeurjc.ais.unit_test;
 
 import es.codeurjc.ais.book.BookService;
+import es.codeurjc.ais.book.OpenLibraryService.BookData;
+import es.codeurjc.ais.book.OpenLibraryService;
+import es.codeurjc.ais.notification.NotificationService;
 import org.junit.jupiter.api.Test;
 
-import static org.mockito.Mockito.mock;
+import java.util.ArrayList;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.when;
+
 
 public class BookServiceTests {
 
-    @Test
-    public class magicTest(){
-        List<Book>
+   @Test
+    public void magicTest(){
         //Given
-        BookService book=mock(BookService.class);
+        OpenLibraryService open_l=mock(OpenLibraryService.class);
+        NotificationService nser=mock(NotificationService.class);
+
         //When
-        when(book.findAll("Magic")).thenReturn();
+        List<BookData> libros=new ArrayList<>();
+        Integer[] auxint={1,2,3};
+        String[] auxStr={"a","b","c"};
+        BookData bk1=new BookData("magia1","clave1","Libro de magia",auxint,auxStr);
+        BookData bk2=new BookData("magia2","clave2","Libro de magi2",auxint,auxStr);
+        libros.add(bk1);
+        libros.add(bk2);
+
+        BookService book=new BookService(open_l,nser);
+        when(open_l.searchBooks(anyString(),anyInt())).thenReturn(libros);
         //Then
+       assertTrue(book.findAll("magic").size()==2);
+
     }
 }
