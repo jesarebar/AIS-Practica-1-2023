2de2814e39631b7a9228cfc2ca567f3661e6a8af
jesarebar <jesarebar@gmail.com>
2023-03-05 19:05:42 +0100

    Segundo test de apiREST funcionando (POR FIN era bien jodido)

--- src/test/java/es/codeurjc/ais/API_REST/RESTAssuredTests.java
+++ src/test/java/es/codeurjc/ais/API_REST/RESTAssuredTests.java
@@ -1,10 +1,19 @@
 package es.codeurjc.ais.API_REST;
 
+import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.bouncycastle.tsp.TSPUtil;
+import org.json.JSONException;
 import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.json.Json;
+
+import java.util.ArrayList;
 
 import static io.restassured.RestAssured.*;
 import static io.restassured.matcher.RestAssuredMatchers.*;
+import static io.restassured.path.json.JsonPath.from;
 import static org.hamcrest.Matchers.*;
 
 public class RESTAssuredTests {
@@ -13,5 +22,33 @@ public class RESTAssuredTests {
         given().param("topic", "drama").
                 when().get("http://localhost:8080/api/books/").
                 then().statusCode(200).body("size()", equalTo(10));
+    }
+
+    @Test
+    public void test2() throws JSONException {
+
+        Response fantasyResponse = given().param("topic", "fantasy")
+                .when().get("http://localhost:8080/api/books/").then().extract().response();
+
+        JsonPath json_fantasyResponse = from(fantasyResponse.getBody().asString());
+
+        ArrayList<String> idList = json_fantasyResponse.get("id");
+
+        String firstBookid = idList.get(0);
+
+        JSONObject review = new JSONObject();
+
+        review.put("id", "1");
+        review.put("nickname", "Tom");
+        review.put("content", "I liked it");
+        review.put("bookId", firstBookid);
+
+        given().request().body(review.toString()).contentType(ContentType.JSON).
+                when().post("http://localhost:8080/api/books/"+firstBookid+"/review").
+                then().assertThat().statusCode(201);
+
+        given().get("http://localhost:8080/api/books/"+firstBookid).
+                then().assertThat().body("reviews", notNullValue());
+
     }
 }
